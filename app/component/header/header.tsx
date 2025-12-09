3378a4529134cced5adcc4af394def71b0e8d01a
ishimaruryouto <2240006@ecc.ac.jp>
2025-12-09 15:29:11 +0900

    [feat]headerの完成

--- app/component/header/header.tsx
+++ app/component/header/header.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
 import styles from "./header.module.scss";
 
 export default function Header() {
     return (
         <>
-
+            <header className={styles.header}>
+                <nav className={styles.nav}>
+                    <Link href="#">ロゴ</Link>
+                    <ul>
+                        <li><Link href="#">柏島について</Link></li>
+                        <li><Link href="#">アクセス</Link></li>
+                    </ul>
+                </nav>
+            </header>
         </>
     );
 }
